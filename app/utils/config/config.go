4c6d89a0fe32327366966155d07a2df48e0eb0ea
madhusudhanv <madhusudhan.vundela@imaginea.com>
2017-02-01 16:45:09 +0530

    Added configuration support using viper

---
+++ app/utils/config/config.go
@@ -0,0 +1,24 @@
+package config
+
+import (
+	"os"
+
+	"github.com/spf13/viper"
+	"github.com/madhusudhannsn/go-web-app/app/utils/error"
+	"github.com/madhusudhannsn/go-web-app/app/utils/logger"
+)
+
+//Init : This method loads the configuration
+func Init() *error.AppError {
+	logger.Info.Println("Initializing the configuration")
+	fileName := os.Getenv("env")
+	viper.SetConfigName(fileName)
+	viper.AddConfigPath("./config")
+	err := viper.ReadInConfig()
+	if err != nil {
+		return error.GetInstance("Config loading error", 1002, err)
+	} else {
+		return nil
+	}
+
+}
